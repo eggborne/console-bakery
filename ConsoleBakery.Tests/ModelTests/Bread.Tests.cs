16a4c94d96f994e07f47604ad8bc5b16b876da45
Mike Donovan <mike@mikedonovan.dev>
2023-02-24 15:29:28 -0800

    Add test for CalculateFreeUnits

--- ConsoleBakery.Tests/ModelTests/Bread.Tests.cs
+++ ConsoleBakery.Tests/ModelTests/Bread.Tests.cs
@@ -7,15 +7,27 @@ namespace ConsoleBakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void PriceForQuantity_ReturnsPriceWithDiscounts_Float()
-    {
+    public void PriceForQuantity_ReturnsPriceWithDiscounts_Float() {
+      Bread tooFew = new Bread(2);
+      Bread earnedOne = new Bread(3);
+      Bread earnedOneButNotTwo = new Bread(5);
+      Bread earnedTwo = new Bread(6);
+
+      Assert.AreEqual(tooFew.PriceForQuantity(), 10.00f);
+      Assert.AreEqual(earnedOne.PriceForQuantity(), 10.00f);
+      Assert.AreEqual(earnedOneButNotTwo.PriceForQuantity(), 20.00f);
+      Assert.AreEqual(earnedTwo.PriceForQuantity(), 20.00f);
+    }
+    public void CalculateFreeUnits_ReturnsCorrectFreeUnits_Int() {
       Bread tooFew = new Bread(1);
       Bread earnedOne = new Bread(3);
       Bread earnedOneButNotTwo = new Bread(4);
+      Bread earnedTwo = new Bread(6);
 
-      Assert.AreEqual(tooFew.PriceForQuantity(), 5.00f);
-      Assert.AreEqual(earnedOne.PriceForQuantity(), 10.00f);
-      Assert.AreEqual(earnedOneButNotTwo.PriceForQuantity(), 15.00f);
+      Assert.AreEqual(tooFew.CalculateFreeUnits(), 0);
+      Assert.AreEqual(earnedOne.CalculateFreeUnits(), 1);
+      Assert.AreEqual(earnedOneButNotTwo.CalculateFreeUnits(), 1);
+      Assert.AreEqual(earnedTwo.CalculateFreeUnits(), 2);
     }
   }
 }
--- ConsoleBakery.Tests/ModelTests/Pastry.Tests.cs
+++ ConsoleBakery.Tests/ModelTests/Pastry.Tests.cs
@@ -7,15 +7,17 @@ namespace ConsoleBakery.Tests
   public class PastryTests
   {
     [TestMethod]
-    public void PriceForQuantity_ReturnsPriceWithDiscounts_Float()
-    {
+    public void PriceForQuantity_ReturnsPriceWithDiscounts_Float() {
       Pastry tooFew = new Pastry(2);
       Pastry earnedOne = new Pastry(4);
-      Pastry earnedOneButNotTwo = new Pastry(5);
+      Pastry earnedOneButNotTwo = new Pastry(7);
+      Pastry earnedTwo = new Pastry(8);
 
       Assert.AreEqual(tooFew.PriceForQuantity(), 4.00f);
       Assert.AreEqual(earnedOne.PriceForQuantity(), 6.00f);
-      Assert.AreEqual(earnedOneButNotTwo.PriceForQuantity(), 8.00f);
+      Assert.AreEqual(earnedOneButNotTwo.PriceForQuantity(), 12.00f);
+      Assert.AreEqual(earnedTwo.PriceForQuantity(), 12.00f);
+      
     }
   }
 }
